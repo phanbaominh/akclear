aa0020ed61ff5c13436b2bfc80b8b736db31a499
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2022-04-11 22:50:45 +0700

    Add basic header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./app/**/*.{html,js,erb}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          400: '#212121',
+          100: '#484848',
+          900: '#000000'
+        },
+        secondary: {
+          100: '#73e8ff',
+          400: '#29b6f6',
+          900: '#0086C3'
+        }
+      }
+    },
   },
   plugins: [],
 }
