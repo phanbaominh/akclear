46922ef96ebf1f956bb0f14b1ab697b4c2c59cbd
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-01-23 11:41:49 +0700

    Add service to fetch JSON

---
+++ app/services/fetch_json.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class FetchJson < ApplicationService
+  def initialize(source)
+    @source = source
+  end
+
+  def call
+    file = URI.parse(source).open
+    raw_data = file.read
+    data = JSON.parse(raw_data)
+
+    Success(data)
+  end
+
+  private
+
+  attr_reader :source
+end
