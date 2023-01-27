49796c36cd7be6a28d52bcb277940201f21483db
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-01-27 20:29:17 +0700

    Update associations for Clear and tests

--- app/models/clear.rb
+++ app/models/clear.rb
@@ -1,5 +1,5 @@
 class Clear < ApplicationRecord
-  belongs_to :uploader
+  belongs_to :submitter, class_name: 'User'
   belongs_to :stage
-  belongs_to :player
+  belongs_to :player, class_name: 'User'
 end
--- spec/models/clear_spec.rb
+++ spec/models/clear_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Clear, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:submitter) }
+    it { is_expected.to belong_to(:stage) }
+    it { is_expected.to belong_to(:player) }
+  end
 end
