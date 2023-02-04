d12b8dee72be73ac06c00fceb744847d3bbb18c3
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-02-04 17:25:48 +0700

    Config ViewComponent js files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import RubyPlugin from "vite-plugin-ruby";
 
 export default defineConfig({
-  plugins: [
-    RubyPlugin(),
-  ],
-})
+  plugins: [RubyPlugin()],
+  resolve: {
+    alias: {
+      "@components": resolve(__dirname, "app/components"),
+    },
+  },
+});
