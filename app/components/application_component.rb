09e5d9021a102785e9ec83b9f2fd6d60771fd8e9
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-02-19 17:31:51 +0700

    Use class as paramater name for component

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationComponent < ViewComponent::Base
-  def initialize(classes: '', id: '', **options)
-    @classes = classes
+  def initialize(class: '', id: '', **options)
+    @class = binding.local_variable_get(:class)
     @id = id
     post_initialize(**options)
   end
