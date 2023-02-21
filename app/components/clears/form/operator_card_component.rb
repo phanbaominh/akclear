713fa7faededc646c34816b1b638420cf1c4bd18
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-02-21 23:20:29 +0700

    Update select operators style

--- app/components/clears/form/operator_card_component.rb
+++ app/components/clears/form/operator_card_component.rb
@@ -4,4 +4,20 @@ class Clears::Form::OperatorCardComponent < ApplicationComponent
   def post_initialize(form:)
     @form = form
   end
+
+  def used_operator
+    @form.object
+  end
+
+  def operator_image_url
+    used_operator.operator.avatar
+  end
+
+  def operator_name
+    used_operator.operator.name
+  end
+
+  def tooltip_id
+    "operator_#{used_operator.operator_id}"
+  end
 end
