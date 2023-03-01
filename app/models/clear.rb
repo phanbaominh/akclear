7368aba4a9fd1c86498560e51413810c2b719fa2
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-03-01 23:15:18 +0700

    Add seed file

--- app/models/clear.rb
+++ app/models/clear.rb
@@ -4,7 +4,7 @@ class Clear < ApplicationRecord
   include Clear::Squadable
   belongs_to :submitter, class_name: 'User'
   belongs_to :stage
-  belongs_to :player, class_name: 'User'
+  belongs_to :player, class_name: 'User', optional: true
   has_many :used_operators, dependent: :destroy
   accepts_nested_attributes_for :used_operators, allow_destroy: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,162 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
 
-def create_used_operator
-  
+def params_for_1_and_2_stars_operator
+  {
+    level: Faker::Number.between(from: 1, to: 30)
+  }
+end
+
+def params_for_3_stars_operator
+  params = {}
+  params[:elite] = Faker::Number.between(from: 0, to: 1)
+  if params[:elite] == 0
+    params.merge!(
+      level: Faker::Number.between(from: 1, to: 40),
+      skill: 1,
+      skill_level: Faker::Number.between(from: 1, to: 4)
+    )
+  else
+    params.merge!(
+      level: Faker::Number.between(from: 1, to: 55),
+      skill: 1,
+      skill_level: Faker::Number.between(from: 1, to: 7)
+    )
+  end
+end
+
+def params_for_4_stars_operator
+  params = {}
+  params[:elite] = Faker::Number.between(from: 0, to: 2)
+  case params[:elite]
+  when 0
+    params.merge!(
+      level: Faker::Number.between(from: 1, to: 45),
+      skill: 1,
+      skill_level: Faker::Number.between(from: 1, to: 4)
+    )
+  when 1
+    params.merge!(
+      level: Faker::Number.between(from: 1, to: 60),
+      skill: Faker::Number.between(from: 1, to: 2),
+      skill_level: Faker::Number.between(from: 1, to: 7)
+    )
+  when 2
+    params.merge!(
+      level: Faker::Number.between(from: 1, to: 70),
+      skill: Faker::Number.between(from: 1, to: 2),
+      skill_level: Faker::Number.between(from: 1, to: 7),
+      used_module: Faker::Number.between(from: 0, to: 1)
+    )
+    params[:skill_mastery] = Faker::Number.between(from: 0, to: 3) if params[:skill_level] == 7
+    params[:module_level] = Faker::Number.between(from: 1, to: 3) if params[:used_module] == 1
+    params
+  end
+end
+
+def params_for_5_stars_operator
+  params = {}
+  params[:elite] = Faker::Number.between(from: 0, to: 2)
+  case params[:elite]
+  when 0
+    params.merge!(
+      level: Faker::Number.between(from: 1, to: 50),
+      skill: 1,
+      skill_level: Faker::Number.between(from: 1, to: 4)
+    )
+  when 1
+    params.merge!(
+      level: Faker::Number.between(from: 1, to: 70),
+      skill: Faker::Number.between(from: 1, to: 2),
+      skill_level: Faker::Number.between(from: 1, to: 7)
+    )
+  when 2
+    params.merge!(
+      level: Faker::Number.between(from: 1, to: 80),
+      skill: Faker::Number.between(from: 1, to: 2),
+      skill_level: Faker::Number.between(from: 1, to: 7),
+      used_module: Faker::Number.between(from: 0, to: 1)
+    )
+    params[:skill_mastery] = Faker::Number.between(from: 0, to: 3) if params[:skill_level] == 7
+    params[:module_level] = Faker::Number.between(from: 1, to: 3) if params[:used_module] == 1
+    params
+  end
+end
+
+def params_for_6_stars_operator
+  params = {}
+  params[:elite] = Faker::Number.between(from: 0, to: 2)
+  case params[:elite]
+  when 0
+    params.merge!(
+      level: Faker::Number.between(from: 1, to: 50),
+      skill: 1,
+      skill_level: Faker::Number.between(from: 1, to: 4)
+    )
+  when 1
+    params.merge!(
+      level: Faker::Number.between(from: 1, to: 80),
+      skill: Faker::Number.between(from: 1, to: 2),
+      skill_level: Faker::Number.between(from: 1, to: 7)
+    )
+  when 2
+    params.merge!(
+      level: Faker::Number.between(from: 1, to: 90),
+      skill: Faker::Number.between(from: 1, to: 3),
+      skill_level: Faker::Number.between(from: 1, to: 7),
+      used_module: Faker::Number.between(from: 0, to: 2)
+    )
+    params[:skill_mastery] = Faker::Number.between(from: 0, to: 3) if params[:skill_level] == 7
+    params[:module_level] = Faker::Number.between(from: 1, to: 3) if params[:used_module].positive?
+    params
+  end
+end
+
+def params_for_used_operator(operator)
+  if operator.rarity < 3
+    params_for_1_and_2_stars_operator
+  elsif operator.rarity == 3
+    params_for_3_stars_operator
+  elsif operator.rarity == 4
+    params_for_4_stars_operator
+  elsif operator.rarity == 5
+    params_for_5_stars_operator
+  else
+    params_for_6_stars_operator
+  end.merge(operator:)
+end
+
+def operators
+  @operators ||= Operator.all
+end
+
+def stages
+  @stages ||= Stage.all
+end
+
+def create_clear(user)
+  num_of_operators = Faker::Number.between(from: 1, to: 13)
+  params = { used_operators_attributes: [] }
+  operators.sample(num_of_operators).each do |operator|
+    params[:used_operators_attributes] << params_for_used_operator(operator)
+  end
+  params.merge!(
+    stage: stages.sample,
+    player_name: Faker::Internet.username,
+    name: Faker::Lorem.sentence,
+    link: 'https://www.youtube.com/watch?v=FNCWoNnFUdc',
+    submitter: user
+  )
+  Clear.create!(params)
+end
+
+if Rails.env.development?
+  user = User.first
+  # FetchGameData::FetchLatestOperatorsData.call
+  # FetchGameData::FetchLatestEventsData.call
+  # FetchGameData::FetchLatestEpisodesData.call
+  # FetchGameData::FetchLatestStagesData.call
+  100.times { create_clear(user) }
 end
