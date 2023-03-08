88dd41098ae257e01ed9a132f15f7260961f0ace
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-03-08 21:45:24 +0700

    Clean up responsive of index page

--- app/models/used_operator.rb
+++ app/models/used_operator.rb
@@ -2,4 +2,7 @@ class UsedOperator < ApplicationRecord
   belongs_to :operator
   belongs_to :clear
   attribute :need_to_be_destroyed, :boolean
+
+  delegate :avatar, :name, to: :operator
+  # TODO: add default values for skill/module
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,19 @@ module.exports = {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        "base-content-subdued": "hsl(var(--bc) / 0.8)",
+      },
+    },
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
     },
   },
   plugins: [
     // require("@tailwindcss/forms"),
     // require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
     require("@tailwindcss/typography"),
     require("daisyui"),
   ],
