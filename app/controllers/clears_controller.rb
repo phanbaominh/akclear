12d0f150ab33ec23151f19e6489aa92564a7d0af
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-03-12 22:08:51 +0700

    Connect filter to clears list

--- app/controllers/clears_controller.rb
+++ app/controllers/clears_controller.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class ClearsController < ApplicationController
+  include Pagy::Backend
+
+  def index
+    @pagy, @clears = pagy(Clear.all.includes(used_operators: :operator, stage: :stageable))
+    @searched_clear = Clear.new(clear_params)
+  end
+
   def new
     @clear = Clear.new(clear_params)
     respond_to do |format|
@@ -20,13 +27,18 @@ class ClearsController < ApplicationController
 
   private
 
+  def index_params
+    params.permit(:stage_id, :operators, :tags)
+  end
+
   def clear_params
-    return nil if action_name == 'new' && params[:clear].nil?
+    return nil if action_name != 'create' && params[:clear].nil?
 
     @clear_params = params
                     .require(:clear)
                     .permit(:name, :link, :stage_id, used_operators_attributes: %i[id operator_id _destroy
-                                                                                   need_to_be_destroyed level])
+                                                                                   need_to_be_destroyed level elite
+                                                                                   skill_level skill_mastery skill])
 
     @clear_params.merge(submitter_id: Current.user.id)
   end
