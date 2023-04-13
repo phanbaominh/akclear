0886d6bc915558e2170999908907fe4f33c9de12
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-04-13 23:07:25 +0700

    DRY stimulus definition logic

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -8,4 +8,38 @@ class ApplicationComponent < ViewComponent::Base
   end
 
   def post_initialize(**options); end
+
+  def stimulus(controller, **attributes)
+    { controller: controller.to_s.dasherize, **stimulus_attrs(controller, attributes) }
+  end
+
+  def stimulus_attrs(controller, **attributes)
+    stimulus_controller = controller.to_s.dasherize
+
+    action_data_attribute = attributes.delete(:actions)&.map do |event, function|
+      "#{event}->#{stimulus_controller}##{function.camelize(:lower)}"
+    end&.join(' ').presence
+
+    { action: action_data_attribute, **stimulus_data_attributes(controller, attributes) }
+  end
+
+  private
+
+  def stimulus_data_attributes(controller, attributes)
+    data_attributes = {}
+    stimulus_data_attributes_mapping.each do |attribute_type, suffix|
+      attributes[attribute_type]&.transform_keys! { |key| :"#{controller}_#{key}_#{suffix}" }
+      data_attributes.merge!(attributes[attribute_type] || {})
+    end
+    data_attributes
+  end
+
+  def stimulus_data_attributes_mapping
+    {
+      values: 'value',
+      outlets: 'outlet',
+      classes: 'class',
+      params: 'param'
+    }
+  end
 end
