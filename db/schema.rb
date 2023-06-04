ead90ca1aa4b25ad907c2965141ecb2996d91b2d
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-06-04 15:40:56 +0700

    feat: add user_name to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_04_083626) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_04_084005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -185,6 +185,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_04_083626) do
     t.boolean "verified", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
