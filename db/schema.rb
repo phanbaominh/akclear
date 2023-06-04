76c8d3767f52a4aa4f321874eda4affe35acf63c
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-06-04 15:38:50 +0700

    feat: add Channel model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_03_095413) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_04_083626) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_03_095413) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "end_time"
+  end
+
+  create_table "channels", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_channels_on_user_id"
   end
 
   create_table "clears", force: :cascade do |t|
@@ -190,6 +199,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_03_095413) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "channels", "users"
   add_foreign_key "clears", "stages"
   add_foreign_key "clears", "users", column: "player_id"
   add_foreign_key "clears", "users", column: "submitter_id"
