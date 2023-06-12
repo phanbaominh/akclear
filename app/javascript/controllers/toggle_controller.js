8d8a1c75d1b87314621981c7fe0824b254f443df
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-06-12 23:23:28 +0700

    fix: update toggle controller and remove cursor flicker for like button

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,16 +2,18 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="clears--form"
 export default class extends Controller {
   static targets = ["toggle"];
+  static classes = ["on", "off"];
 
   toggle() {
-    this.toggleTargets.forEach((target) => {
-      if (target.dataset.toggled == "true") {
-        target.classList.add("hidden");
-        target.dataset.toggled = false;
-      } else {
-        target.classList.remove("hidden");
-        target.dataset.toggled = true;
-      }
-    });
+    console.log(this.toggle)
+    if (this.toggleTarget.dataset.toggled == "true") {
+      this.toggleTarget.classList.remove(...this.onClasses);
+      this.toggleTarget.classList.add(...this.offClasses);
+      this.toggleTarget.dataset.toggled = "false";
+    } else {
+      this.toggleTarget.classList.remove(...this.offClasses);
+      this.toggleTarget.classList.add(...this.onClasses);
+      this.toggleTarget.dataset.toggled = "true";
+    }
   }
 }
