2db31fda7fcd11b7559b3b330373f8d597772139
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-06-13 23:40:47 +0700

    fix: update fetching banners to work for all events

--- app/services/fetch_game_data/fetch_event_banners.rb
+++ app/services/fetch_game_data/fetch_event_banners.rb
@@ -6,17 +6,6 @@ module FetchGameData
     CN_EVENTS_DATA_URL = 'https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/activity_table.json'
     BANNERS_PAGE_URL = 'https://prts.wiki/w/%E6%B4%BB%E5%8A%A8%E4%B8%80%E8%A7%88'
     IMAGE_HOST = 'https://prts.wiki'
-    BANNERS_PAGE_EVENTS_SPECIAL_NAMES = {
-      '理想城：长夏狂欢季' => '夏活2022', # Ideal City
-      '密林悍将归来' => '夏活2020', # Gavial the Great Chief Return
-      '多索雷斯假日' => '多索雷斯假日2022', # Dossoles Holiday
-      '踏寻往昔之风' => '金秋2020', # Rewinding Breeze
-      '午间逸话' => '午间逸话活动预告01', # Stories of afternoon
-      '喧闹法则' => '喧闹法则-限时活动', # Code of Brawl
-      '火蓝之心' => '活动预告-火蓝之心活动', # Heart of Surging Flame
-      '战地秘闻' => '活动预告-战地秘闻活动', # Operational Intel
-      '骑兵与猎人' => '骑兵与猎人-预告公告' # Grani
-    }.freeze
 
     def self.images_path
       Rails.root.join('app/javascript/images/banners')
@@ -38,49 +27,34 @@ module FetchGameData
       @events ||= Event.all
     end
 
-    def banners_page_event_name(original_name)
-      BANNERS_PAGE_EVENTS_SPECIAL_NAMES[original_name] || original_name
-    end
-
     def build_cn_event_name_to_game_id(cn_events_data)
       events.each_with_object({}) do |event, mapping|
-        original_event_name = cn_events_data['basicInfo'][event.game_id]['name']
-        event_name = banners_page_event_name(original_event_name)
+        event_name = cn_events_data['basicInfo'][event.game_id]['name']
         mapping[event_name] = event.game_id
       end
     end
 
     def build_event_game_id_to_banner_url(cn_event_name_to_game_id)
       event_game_id_to_banner_url = {}
-      event_day = {}
       log_info('Fetching banners page...')
       doc = Nokogiri::HTML(URI.parse(BANNERS_PAGE_URL).open)
-      doc.css('td > a > img').each do |img|
-        img_event_name, day = extract_event_name_and_day(img[:alt])
-        current_event_day = event_day[img_event_name]
+      doc.css('tr').each do |cell|
+        links = cell.css('td > a')
 
-        next if !day.nil? && !current_event_day.nil? && day > current_event_day
+        next unless links.size >= 3
+
+        img_event_name = links.first.text
 
         game_id = cn_event_name_to_game_id[img_event_name]
         next if game_id.nil?
 
+        img = links.last.css('img').first
+
+        next if img.nil?
+
         event_game_id_to_banner_url[game_id] = "#{IMAGE_HOST}#{img['data-src'].gsub('650', '1300')}"
-        event_day[img_event_name] = day
       end
       event_game_id_to_banner_url
-    end
-
-    def extract_event_name_and_day(img_alt)
-      parts = img_alt.split(' ')
-      if parts.size == 3
-        _, img_event_name, day_with_extension = parts
-        [img_event_name, day_with_extension.split('.').first.to_i]
-      elsif parts.size == 2
-        _, img_event_name_with_extension = parts
-        [img_event_name_with_extension.split('.').first, nil]
-      else
-        [parts.first.split('.').first, nil]
-      end
     end
 
     def image_storable
--- spec/services/fetch_game_data/fetch_event_banners_spec.rb
+++ spec/services/fetch_game_data/fetch_event_banners_spec.rb
@@ -5,24 +5,13 @@ describe FetchGameData::FetchEventBanners do
   let_it_be(:ideal_city_summer_event) do
     create(:event, game_id: 'test_act20side')
   end
-  let_it_be(:dossoles_holiday_event) do
-    create(:event, game_id: 'test_act12side')
-  end
-  let_it_be(:invitation_to_wines_event) do
-    create(:event, game_id: 'test_act15side')
-  end
   let(:cn_events_data) do
     {
       'basicInfo' => {
         'test_act20side' => {
           'name' => '理想城：长夏狂欢季'
         },
-        'test_act12side' => {
-          'name' => '多索雷斯假日'
-        },
-        'test_act15side' => {
-          'name' => '将进酒'
-        },
+
         'non-existing' => {
           'name' => 'abc'
         }
@@ -32,33 +21,21 @@ describe FetchGameData::FetchEventBanners do
 
   let(:banners_page) do
     <<~HTML
-      <td>
-        <a>
-          <img alt="活动预告 夏活2022 02.jpg"
-          data-src="/images/thumb/f/fb/%E6%B4%BB%E5%8A%A8%E9%A2%84%E5%91%8A_%E5%9B%9B%E5%91%A8%E5%B9%B4%E5%BA%86%E5%85%B8_29.jpg/650px-%E6%B4%BB%E5%8A%A8%E9%A2%84%E5%91%8A_%E5%9B%9B%E5%91%A8%E5%B9%B4%E5%BA%86%E5%85%B8_29.jpg">
-        </a>
-      </td>
-      <img alt="多索雷斯假日2022.jpg" data-src="/images/650_test_act12side">
-      <td>
-        <a>
-          <img alt="多索雷斯假日2022.jpg" data-src="/images/650_test_act12side">
-        </a>
-      </td>
-      <td>
-      <a>
-        <img alt="活动预告 将进酒 15.jpg" data-src="650_test_act15side15">
-      </a>
-      </td>
-      <td>
-        <a>
-          <img alt="活动预告 将进酒 01.jpg" data-src="/images/650_test_act15side1">
-        </a>
-      </td>
-      <td>
-        <a>
-          <img alt="活动预告 abc 01.jpg" data-src="650_test_act15side1">
-        </a>
-      </td>
+      <tr style="">
+        <td>2022-01-25 16:00
+        </td>
+        <td><a>理想城：长夏狂欢季</a>
+        </td>
+        <td>
+          <a></a>
+        </td>
+        <td>
+          <a>
+            <img
+            data-src="/images/thumb/f/fb/%E6%B4%BB%E5%8A%A8%E9%A2%84%E5%91%8A_%E5%9B%9B%E5%91%A8%E5%B9%B4%E5%BA%86%E5%85%B8_29.jpg/650px-%E6%B4%BB%E5%8A%A8%E9%A2%84%E5%91%8A_%E5%9B%9B%E5%91%A8%E5%B9%B4%E5%BA%86%E5%85%B8_29.jpg">
+          </a>
+        </td>
+      </tr>
     HTML
   end
   let(:service) { described_class.new }
@@ -77,20 +54,12 @@ describe FetchGameData::FetchEventBanners do
     service.call
 
     expect(URI).to have_received(:parse).with('https://prts.wiki/images/thumb/f/fb/%E6%B4%BB%E5%8A%A8%E9%A2%84%E5%91%8A_%E5%9B%9B%E5%91%A8%E5%B9%B4%E5%BA%86%E5%85%B8_29.jpg/1300px-%E6%B4%BB%E5%8A%A8%E9%A2%84%E5%91%8A_%E5%9B%9B%E5%91%A8%E5%B9%B4%E5%BA%86%E5%85%B8_29.jpg')
-    expect(URI).to have_received(:parse).with('https://prts.wiki/images/1300_test_act12side')
-    expect(URI).to have_received(:parse).with('https://prts.wiki/images/1300_test_act15side1')
-    expect(URI).to have_received(:parse).exactly(4).times
+    expect(URI).to have_received(:parse).exactly(2).times
 
     expect(IO)
       .to have_received(:copy_stream)
       .with('image file', Regexp.new('app/javascript/images/banners/test_act20side.jpg'))
-    expect(IO)
-      .to have_received(:copy_stream)
-      .with('image file', Regexp.new('app/javascript/images/banners/test_act12side.jpg'))
-    expect(IO)
-      .to have_received(:copy_stream)
-      .with('image file', Regexp.new('app/javascript/images/banners/test_act15side.jpg'))
-    expect(IO).to have_received(:copy_stream).exactly(3).times
+    expect(IO).to have_received(:copy_stream).exactly(1).times
   end
 
   include_examples 'image_storable' do
