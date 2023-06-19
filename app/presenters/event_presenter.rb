578d90e0d9ac55ea9558f17a22a29ace78ec7210
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-06-19 23:04:38 +0700

    fix: use correct filter link for rerun event

--- app/presenters/event_presenter.rb
+++ app/presenters/event_presenter.rb
@@ -1,4 +1,12 @@
 class EventPresenter < StageablePresenter
+  def filter_link
+    if object.rerun_event?
+      object.original_event.filter_link
+    else
+      super
+    end
+  end
+
   def type_color
     '-warning'
   end
