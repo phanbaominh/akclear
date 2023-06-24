042e0b9cc6afabf1988486fd589d66c377d9ac08
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-06-24 23:08:35 +0700

    feat: run fetch data task during render deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,7 @@ set -o errexit
 
 bundle install
 npm install
+bundle exec rake fetch_latest_game_data:all
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
