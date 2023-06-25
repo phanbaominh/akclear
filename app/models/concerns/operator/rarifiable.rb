991b53125262e5154cb751954b52c186dc588f22
MINH PHAN BAO <minh.phanbao@employmenthero.com>
2023-06-25 15:29:08 +0700

    feat: add rarifiable concern for operaetor

---
+++ app/models/concerns/operator/rarifiable.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+module Operator::Rarifiable
+  extend ActiveSupport::Concern
+
+  RARITIES = [
+    ONE_STAR = 'one_star',
+    TWO_STARS = 'two_stars',
+    THREE_STARS = 'three_stars',
+    FOUR_STARS = 'four_stars',
+    FIVE_STARS = 'five_stars',
+    SIX_STARS = 'six_stars'
+  ].freeze
+
+  # convert the below array to a hash with keys as the rarity and values as the max elite
+  RARITIES_TO_MAX_ELITE = {
+    ONE_STAR => 0,
+    TWO_STARS => 0,
+    THREE_STARS => 1,
+    FOUR_STARS => 2,
+    FIVE_STARS => 2,
+    SIX_STARS => 2
+  }.freeze
+  RARITIES_TO_NUMBER_OF_SKILLS = {
+    ONE_STAR => 0,
+    TWO_STARS => 0,
+    THREE_STARS => 1,
+    FOUR_STARS => 2,
+    FIVE_STARS => 2,
+    SIX_STARS => 3
+  }.freeze
+  RARITIES_TO_MAX_LEVEL = {
+    ONE_STAR => [30],
+    TWO_STARS => [30],
+    THREE_STARS => [40, 55],
+    FOUR_STARS => [45, 60, 70],
+    FIVE_STARS => [50, 70, 80],
+    SIX_STARS => [50, 80, 90]
+  }.freeze
+  RARITIES_TO_MAX_SKILL_LEVEL = {
+    ONE_STAR => [0],
+    TWO_STARS => [0],
+    THREE_STARS => [4, 7],
+    FOUR_STARS => [4, 7, 7],
+    FIVE_STARS => [4, 7, 7],
+    SIX_STARS => [4, 7, 7]
+  }.freeze
+
+  included do
+    enum :rarity, RARITIES
+  end
+
+  def max_elite
+    RARITIES_TO_MAX_ELITE[rarity]
+  end
+
+  def number_of_skills
+    RARITIES_TO_NUMBER_OF_SKILLS[rarity]
+  end
+
+  def max_level(elite:)
+    RARITIES_TO_MAX_LEVEL[rarity][elite]
+  end
+
+  def max_skill_level(elite:)
+    RARITIES_TO_MAX_SKILL_LEVEL[rarity][elite]
+  end
+end
