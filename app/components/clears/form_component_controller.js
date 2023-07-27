5b1d489421c7af26e2617641581a6b16ec22b732
Phan Bao Minh <pbminh2511@gmail.com>
2023-07-27 19:13:53 +0700

    feat: show youtube iframe after entering link

--- app/components/clears/form_component_controller.js
+++ app/components/clears/form_component_controller.js
@@ -1,5 +1,12 @@
 import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
+  static targets = ["link", "videoFrame"];
+
+  hideVideoFrame() {
+    this.videoFrameTarget.classList.add("hidden");
+    this.videoFrameTarget.src = "";
+  }
+
   highlight({ detail: { operatorId } }) {
     this.element.querySelectorAll(".operators__card").forEach((card) => {
       card.classList.remove("outline", "outline-primary");
@@ -7,5 +14,17 @@ export default class extends Controller {
         card.classList.add("outline", "outline-primary");
       }
     });
+  }
+  embedLink() {
+    if (!this.linkTarget.value.match(/http.*youtube.*watch\?v=/)) {
+      this.hideVideoFrame();
+      return;
+    }
+
+    this.videoFrameTarget.classList.remove("hidden");
+    this.videoFrameTarget.src = this.linkTarget.value.replace(
+      /watch\?v=/,
+      "embed/"
+    );
   }
 }
