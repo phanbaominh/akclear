42c97781aedcc03656a885f8411779db1189492d
Phan Bao Minh <pbminh2511@gmail.com>
2023-07-29 08:40:15 +0700

    refactor: move clear specification logic into concern

--- app/models/clear.rb
+++ app/models/clear.rb
@@ -3,6 +3,7 @@ class Clear < ApplicationRecord
   include Clear::HardTaggable
   include Clear::Squadable
   include Clear::Likeable
+  include Clear::Specifiable
   include Youtubeable
   belongs_to :submitter, class_name: 'User'
   belongs_to :stage
@@ -16,50 +17,8 @@ class Clear < ApplicationRecord
 
   validates :link, presence: true
 
-  attribute :challenge_mode, :boolean
-  attribute :environment, :string
-  attribute :stageable_id, :string
-  attribute :operator_id, :integer
-  attribute :stage_type, :string
-
-  validates :stage_type, inclusion: { in: Stage::STAGEABLE_TYPES }, allow_blank: true
-
-  def environment
-    Episode::Environment.new(super) if super
-  end
-
-  def stageable
-    stageable_id ? GlobalID::Locator.locate(stageable_id) : stage&.stageable
-  end
-
-  def stage_id
-    stageable.is_a?(Annihilation) ? stageable.stages.first.id : super
-  end
 
   def verified?
     verification.present?
-  end
-
-  def challenge_mode=(value)
-    if stageable&.challengable?
-      super
-    else
-      super nil
-    end
-  end
-
-  def environment=(value)
-    if stageable&.has_environments?
-      super
-    else
-      super nil
-    end
-  end
-
-  def reset_spec
-    self.stage_id = nil
-    self.stageable_id = nil
-    self.environment = nil
-    self.challenge_mode = nil
   end
 end
---
+++ app/models/concerns/clear/specifiable.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module Clear::Specifiable
+  extend ActiveSupport::Concern
+
+  included do
+    attribute :challenge_mode, :boolean
+    attribute :environment, :string
+    attribute :stageable_id, :string
+    attribute :stage_type, :string
+
+    validates :stage_type, inclusion: { in: Stage::STAGEABLE_TYPES }, allow_blank: true
+  end
+
+  def environment
+    Episode::Environment.new(super) if super
+  end
+
+  def stageable
+    stageable_id ? GlobalID::Locator.locate(stageable_id) : stage&.stageable
+  end
+
+  def stage_id
+    stageable.is_a?(Annihilation) ? stageable.stages.first.id : super
+  end
+
+  def challenge_mode=(value)
+    if stageable&.challengable?
+      super
+    else
+      super nil
+    end
+  end
+
+  def environment=(value)
+    if stageable&.has_environments?
+      super
+    else
+      super nil
+    end
+  end
+
+  def reset_spec
+    self.stage_id = nil
+    self.stageable_id = nil
+    self.environment = nil
+    self.challenge_mode = nil
+  end
+end
