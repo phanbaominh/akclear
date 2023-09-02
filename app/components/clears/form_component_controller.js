da7258d6add3808abc7f0f77bdb316367179d4db
Phan Bao Minh <pbminh2511@gmail.com>
2023-09-02 17:19:45 +0700

    fix: support multiple modes of editing operators

--- app/components/clears/form_component_controller.js
+++ app/components/clears/form_component_controller.js
@@ -1,10 +1,15 @@
 import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
-  static targets = ["link", "videoFrame"];
+  static targets = ["link", "videoFrame", "toggleVideoFrame"];
 
-  hideVideoFrame() {
-    this.videoFrameTarget.classList.add("hidden");
-    this.videoFrameTarget.src = "";
+  connect() {
+    this.element.querySelector("input[aria-label='Basic']").click();
+  }
+
+  hideVideoFrame(event, target = null) {
+    target = target || this.videoFrameTarget;
+    target.classList.add("hidden");
+    target.src = "";
   }
 
   highlight({ detail: { operatorId } }) {
@@ -15,16 +20,38 @@ export default class extends Controller {
       }
     });
   }
-  embedLink() {
+  embedLink(event, target = null) {
+    target = target || this.videoFrameTarget;
     if (!this.linkTarget.value.match(/http.*youtube.*watch\?v=/)) {
       this.hideVideoFrame();
       return;
     }
 
-    this.videoFrameTarget.classList.remove("hidden");
-    this.videoFrameTarget.src = this.linkTarget.value.replace(
-      /watch\?v=/,
-      "embed/"
-    );
+    target.classList.remove("hidden");
+    target.src = this.linkTarget.value.replace(/watch\?v=/, "embed/");
+  }
+
+  toggleShowVideo(event) {
+    if (event.target.checked) {
+      this.embedLink(event, this.toggleVideoFrameTarget);
+    } else {
+      this.hideVideoFrame(event, this.toggleVideoFrameTarget);
+    }
+  }
+
+  switchView(event) {
+    if (event.target.value == "detailed") {
+      this.element
+        .querySelector("#detailed_operators")
+        .classList.remove("sm:hidden");
+      this.element
+        .querySelector("#basic_operators")
+        .classList.remove("sm:block");
+    } else {
+      this.element
+        .querySelector("#detailed_operators")
+        .classList.add("sm:hidden");
+      this.element.querySelector("#basic_operators").classList.add("sm:block");
+    }
   }
 }
