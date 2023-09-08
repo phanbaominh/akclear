f2e0fcce97bdc39ddc4cfd17852856eb3461bc48
Phan Bao Minh <pbminh2511@gmail.com>
2023-09-08 17:55:04 +0700

    fix: redirect to new clear form when clear is created from job

--- app/controllers/clears/from_jobs_controller.rb
+++ app/controllers/clears/from_jobs_controller.rb
@@ -3,6 +3,8 @@ module Clears
     include ClearFilterable
     def new
       @job = ExtractClearDataFromVideoJob.find_by(id: params[:job_id])
+      return redirect_to(new_clear_path) unless @job.completed?
+
       set_modifying_clear
       @clear = @job.clear
       render 'clears/new'
