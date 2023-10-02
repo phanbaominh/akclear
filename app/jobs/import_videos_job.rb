41849cea551fda1698001ab8c2c46f2c241ee30d
Phan Bao Minh <pbminh2511@gmail.com>
2023-10-02 23:22:20 +0700

    feat: implement import videos job

--- app/jobs/import_videos_job.rb
+++ app/jobs/import_videos_job.rb
@@ -1,7 +1,9 @@
 class ImportVideosJob < ApplicationJob
   queue_as :system
 
-  def perform(*args)
+  def perform(spec_params)
+    spec = Channel::VideosImportSpecification.new(spec_params.transform_keys(&:to_sym))
+    Channel.import_videos(spec)
     # Do something later
   end
 end
