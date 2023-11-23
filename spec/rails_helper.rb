45eeabeeb6e6d7e54e15d71e9d2e07ed8dfc8d82
Phan Bao Minh <pbminh2511@gmail.com>
2023-11-24 00:04:13 +0700

    fix: correctly config Webmock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,6 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 
-WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
