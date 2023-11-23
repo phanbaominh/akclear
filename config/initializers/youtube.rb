3926a06ee9cac18747f2e8938ff5e8c3a75253ec
Phan Bao Minh <pbminh2511@gmail.com>
2023-11-24 00:05:11 +0700

    fix: use ENV insteand of cred for youtube api key

--- config/initializers/youtube.rb
+++ config/initializers/youtube.rb
@@ -1,4 +1,4 @@
 Yt.configure do |config|
-  config.api_key = Rails.application.credentials.youtube_api_key
+  config.api_key = ENV.fetch('YOUTUBE_API_KEY', nil)
   config.log_level = 'devel' unless Rails.env.production?
 end
