a1f304bc799bcca3590776b0ea7cb3240945fbd6
Phan Bao Minh <pbminh2511@gmail.com>
2023-11-30 23:09:39 +0700

    fix: fix minor issue with clear form UI

--- app/components/primer/tabs_component.rb
+++ app/components/primer/tabs_component.rb
@@ -4,7 +4,7 @@ class Primer::TabsComponent < ApplicationComponent
   include Pagy::Frontend
   attr_reader :tab_names, :hidden_class
 
-  renders_many :tabs
+  renders_one :content_with_tabs
 
   def post_initialize(tab_names:, hidden_class: 'hidden')
     @tab_names = tab_names
