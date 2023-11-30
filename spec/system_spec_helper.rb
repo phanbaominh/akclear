5378c88e469c2e1463388278857af0dfca85946d
Phan Bao Minh <pbminh2511@gmail.com>
2023-11-30 23:08:20 +0700

    feat: add option to run spec with browser

--- spec/system_spec_helper.rb
+++ spec/system_spec_helper.rb
@@ -8,14 +8,16 @@ RSpec.configure do |config|
     driven_by :rack_test
   end
 
+  browser = ENV['DEBUG_SYSTEM_SPEC'] ? :chrome : :headless_chrome
+
   config.before(:each, :js, type: :system) do
-    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+    driven_by :selenium, using: browser, screen_size: [1400, 1400]
   end
 
   config.before(:each, :js, :mobile, type: :system) do
     # name option is important, otherwise Capybara won't switch between two same driver types
     # due to how rails system test register (actionpack) all drivers with driver type as name unless specified
-    driven_by :selenium, using: :headless_chrome, screen_size: [375, 667], options: { name: :selenium_mobile }
+    driven_by :selenium, using: browser, screen_size: [375, 667], options: { name: :selenium_mobile }
   end
 
   config.include Helpers::Authentication, type: :system
