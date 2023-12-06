2ff6a47297c5c5e8fad5ced3d94f0a9c764ccc1f
Phan Bao Minh <pbminh2511@gmail.com>
2023-12-07 00:18:22 +0700

    refactor: split filter component into a seperate component

--- app/components/clears/form_component.rb
+++ app/components/clears/form_component.rb
@@ -7,17 +7,16 @@ class Clears::FormComponent < ApplicationComponent
   renders_one :header
   renders_one :footer
 
-  attr_reader :clear_spec, :write_form, :job_id
+  attr_reader :clear_spec, :job_id
 
   delegate :operator_ids, to: :clear_spec
 
-  def post_initialize(clear_spec:, write_form: false, job_id: false)
+  def post_initialize(clear_spec:, job_id: false)
     @clear_spec = clear_spec
-    @write_form = write_form
     @job_id = job_id
   end
 
   def edit_form
-    write_form && clear_spec.persisted?
+    clear_spec.persisted?
   end
 end
