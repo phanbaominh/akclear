4141fd6d4ca7fb147ec83a83bbcb784be6a29736
Phan Bao Minh <pbminh2511@gmail.com>
2023-12-16 18:11:29 +0700

    fix: standardize like component

--- app/components/clears/like_component.rb
+++ app/components/clears/like_component.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 class Clears::LikeComponent < ApplicationComponent
-  attr_reader :clear, :disabled
+  attr_reader :clear, :disabled, :hover_class
 
   delegate :liked?, :likes_count, to: :clear
 
-  def post_initialize(clear:, disabled: false)
+  def post_initialize(clear:, disabled: false, have_hover: true, turbo_id: nil)
     @clear = clear
     @disabled = disabled
+    @hover_class = have_hover ? "px-2 #{'hover:bg-primary/40' unless disabled}" : ''
+    @turbo_id = turbo_id
+  end
+
+  def turbo_id
+    @turbo_id || dom_id(@clear, :like)
   end
 end
