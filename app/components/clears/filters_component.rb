7cf63383121d49cc6fb5e04cdf8a3f8703b06b7a
Phan Bao Minh <pbminh2511@gmail.com>
2023-12-24 15:27:31 +0700

    feat: switch to correct tab when filter redirect from homepage

--- app/components/clears/filters_component.rb
+++ app/components/clears/filters_component.rb
@@ -6,4 +6,12 @@ class Clears::FiltersComponent < ApplicationComponent
   def initialize(clear_spec:)
     @clear_spec = clear_spec
   end
+
+  def stageable_selected?
+    clear_spec.stage_id.blank? && clear_spec.stageable_id.present?
+  end
+
+  def default_tab_name
+    stageable_selected? ? 'detailed' : 'basic'
+  end
 end
--- app/components/primer/tabs_component.rb
+++ app/components/primer/tabs_component.rb
@@ -6,10 +6,10 @@ class Primer::TabsComponent < ApplicationComponent
 
   renders_one :content_with_tabs
 
-  def post_initialize(tab_names:, hidden_class: 'hidden', tab_header_class: '')
+  def post_initialize(tab_names:, hidden_class: 'hidden', tab_header_class: '', default_tab_name: nil)
     @tab_names = tab_names
     @hidden_class = hidden_class
     @tab_header_class = tab_header_class
-    @default_tab_name = tab_names.first
+    @default_tab_name = default_tab_name || tab_names.first
   end
 end
