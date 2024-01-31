090c97d4706a5e2e0442529d629eaae533ac633a
Phan Bao Minh <pbminh2511@gmail.com>
2024-01-31 23:33:02 +0700

    feat: add default config for languageu

--- app/models/clear_image/configuration.rb
+++ app/models/clear_image/configuration.rb
@@ -7,14 +7,35 @@ class ClearImage::Configuration
   attribute :max_largest_card_dist_to_smallest_dist_ratio_to_guess_dist_between_card, :float, default: 1.2
   attribute :max_character_distance_to_width_ratio_to_be_near, :float, default: 1.5
 
+  attr_accessor :language
+
+  DEFAULTS = {
+    en: {
+      word_in_line_second_pass_confidence_threshold: 30
+    }
+  }.freeze
+
+  def initialize(attributes = {})
+    super
+    self.language = attributes[:language]
+    apply_language_specific_defaults
+  end
+
+  def apply_language_specific_defaults
+    return if language.blank?
+
+    DEFAULTS[language.to_sym]&.each do |key, value|
+      send("#{key}=", value)
+    end
+  end
+
   class << self
-    def for_current_thread=(options)
-      options ||= { en: { word_in_line_second_pass_confidence_threshold: 30 } }
+    def for_current_thread=(options = {})
       options = options.with_indifferent_access
       global_options = options[:global] || {}
       Thread.current[:clear_image_configuration] =
         ClearImage::Extracting::Reader::LOCALE_TO_TESSERACT_LANG.keys.map.with_object({}) do |language, lang_to_config|
-          lang_to_config[language] = new(global_options.merge(options[language] || {}))
+          lang_to_config[language] = new(global_options.merge(options[language] || { language: }))
         end
     end
 
