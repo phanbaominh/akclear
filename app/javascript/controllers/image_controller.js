24f8fd88585065dfc87d31681261e0e79ba09bd7
Phan Bao Minh <pbminh2511@gmail.com>
2024-02-17 16:41:24 +0700

    feat: navigate image in test result

--- app/javascript/controllers/image_controller.js
+++ app/javascript/controllers/image_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static outlets = ["image"];
+
   enlarge() {
     this.imageFrame.classList.remove("hidden");
     this.imageFrame.setAttribute("src", this.currentImageSrc);
@@ -11,6 +13,38 @@ export default class extends Controller {
     this.imageFrame.classList.add("hidden");
     this.imageFrame.setAttribute("src", "");
     this.element.classList.remove("outline", "outline-primary");
+  }
+
+  goNext() {
+    this.imageOutlets.forEach((image, index) => {
+      // enlarge next image
+      if (image === this) {
+        const nextImage =
+          this.imageOutlets[(index + 1) % this.imageOutlets.length];
+        if (nextImage) {
+          this.minimize();
+          nextImage.element.focus();
+          nextImage.enlarge();
+          return;
+        }
+      }
+    });
+  }
+
+  goPrev() {
+    this.imageOutlets.forEach((image, index) => {
+      // enlarge next image
+      if (image === this) {
+        if (index == 0) return;
+        const nextImage = this.imageOutlets[index - 1];
+        if (nextImage) {
+          this.minimize();
+          nextImage.element.focus();
+          nextImage.enlarge();
+          return;
+        }
+      }
+    });
   }
 
   get imageFrame() {
